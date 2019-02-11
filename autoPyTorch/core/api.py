e3f141481693ca0261a6deca11f244e952bae1fc
Matthias Urban <urbanm@informatik.uni-freiburg.de>
2019-02-11 13:04:40 +0100

    Better error message when no model is fit due to max_runtime being too small

--- autoPyTorch/core/api.py
+++ autoPyTorch/core/api.py
@@ -108,6 +108,10 @@ class AutoNet():
         self.fit_result = self.pipeline.fit_pipeline(pipeline_config=self.autonet_config,
                                                      X_train=X_train, Y_train=Y_train, X_valid=X_valid, Y_valid=Y_valid)
         self.pipeline.clean()
+
+        if not self.fit_result["optimized_hyperparameter_config"]:
+            raise RuntimeError("No models fit during training, please retry with a larger max_runtime.")
+        
         if (refit):
             self.refit(X_train, Y_train, X_valid, Y_valid)
         return self.fit_result["optimized_hyperparameter_config"], self.fit_result['final_metric_score']
@@ -143,7 +147,7 @@ class AutoNet():
         if (autonet_config is None or hyperparameter_config is None):
             raise ValueError("You have to specify a hyperparameter and autonet config in order to be able to refit")
 
-        assert len(hyperparameter_config) > 0, "You have to specify a non-empty hyperparameter config for refit. Probably something went wrong in fit."
+        assert len(hyperparameter_config) > 0, "You have to specify a non-empty hyperparameter config for refit."
 
         refit_data = {'hyperparameter_config': hyperparameter_config,
                       'budget': budget,
