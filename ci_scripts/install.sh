fcb0cc8a326ded55a0f42ee731b671517355adc0
LMZimmer <50441716+LMZimmer@users.noreply.github.com>
2020-01-27 11:28:18 -0500

    Update install.sh

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -3,7 +3,7 @@ pip --version
 pip install nose pep8 codecov coverage flake8
 
 # Install the packages in the correct order specified by the requirements.txt file
-pip freeze > requirements.txt
+#pip freeze > requirements.txt
 cat requirements.txt | xargs -n 1 -L 1 pip install
 
 # Debug output to know all exact package versions!
