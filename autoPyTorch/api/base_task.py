a59fbcf6576a4482e01384161902d49fbcbcb852
Ravin Kohli <kohliravin7@gmail.com>
2021-01-29 22:07:23 +0100

    added value error for search space updates

--- autoPyTorch/api/base_task.py
+++ autoPyTorch/api/base_task.py
@@ -182,11 +182,10 @@ class BaseTask:
 
         self.search_space_updates = search_space_updates
         if search_space_updates is not None:
-            assert isinstance(self.search_space_updates,
-                              HyperparameterSearchSpaceUpdates), "Expected search space updates to be of instance" \
-                                                                 " HyperparameterSearchSpaceUpdates got {}".format(
-                type(self.search_space_updates)
-            )
+            if not isinstance(self.search_space_updates,
+                              HyperparameterSearchSpaceUpdates):
+                raise ValueError("Expected search space updates to be of instance"
+                                 " HyperparameterSearchSpaceUpdates got {}".format(type(self.search_space_updates)))
 
     @abstractmethod
     def _get_required_dataset_properties(self, dataset: BaseDataset) -> Dict[str, Any]:
