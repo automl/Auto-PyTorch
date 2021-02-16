5a1914068b992caf5fe4323939a1f01a71e4b332
Sebastian Walter <sebastian.walter98@gmail.com>
2021-02-16 16:38:51 +0100

    updated example

--- examples/example_tabular_regression.py
+++ examples/example_tabular_regression.py
@@ -13,6 +13,8 @@ import warnings
 
 from sklearn.datasets import make_regression
 
+from autoPyTorch.data.tabular_feature_validator import TabularFeatureValidator
+
 os.environ['JOBLIB_TEMP_FOLDER'] = tmp.gettempdir()
 os.environ['OMP_NUM_THREADS'] = '1'
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
@@ -26,35 +28,6 @@ from sklearn import model_selection, preprocessing
 from autoPyTorch.api.tabular_regression import TabularRegressionTask
 from autoPyTorch.datasets.tabular_dataset import TabularDataset
 from autoPyTorch.utils.hyperparameter_search_space_update import HyperparameterSearchSpaceUpdates
-
-
-# Get the training data for tabular regression
-def get_data_to_train() -> typing.Tuple[typing.Any, typing.Any, typing.Any, typing.Any]:
-    """
-    This function returns a fit dictionary that within itself, contains all
-    the information to fit a pipeline
-    """
-
-    # Get the training data for tabular regression
-    # X, y = datasets.fetch_openml(name="cholesterol", return_X_y=True)
-
-    # Use dummy data for now since there are problems with categorical columns
-    X, y = make_regression(
-        n_samples=5000,
-        n_features=4,
-        n_informative=3,
-        n_targets=1,
-        shuffle=True,
-        random_state=0
-    )
-
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        X,
-        y,
-        random_state=1,
-    )
-
-    return X_train, X_test, y_train, y_test
 
 
 def get_search_space_updates():
@@ -83,7 +56,25 @@ if __name__ == '__main__':
     ############################################################################
     # Data Loading
     # ============
-    X_train, X_test, y_train, y_test = get_data_to_train()
+
+    # Get the training data for tabular regression
+    # X, y = datasets.fetch_openml(name="cholesterol", return_X_y=True)
+
+    # Use dummy data for now since there are problems with categorical columns
+    X, y = make_regression(
+        n_samples=5000,
+        n_features=4,
+        n_informative=3,
+        n_targets=1,
+        shuffle=True,
+        random_state=0
+    )
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X,
+        y,
+        random_state=1,
+    )
 
     # Scale the regression targets to have zero mean and unit variance.
     # This is important for Neural Networks since predicting large target values would require very large weights.
@@ -94,10 +85,6 @@ if __name__ == '__main__':
     y_train_scaled = (y_train - y_train_mean) / y_train_std
     y_test_scaled = (y_test - y_train_mean) / y_train_std
 
-    datamanager = TabularDataset(
-        X=X_train, Y=y_train_scaled,
-        X_test=X_test, Y_test=y_test_scaled)
-
     ############################################################################
     # Build and fit a regressor
     # ==========================
@@ -105,15 +92,15 @@ if __name__ == '__main__':
         delete_tmp_folder_after_terminate=False,
         search_space_updates=get_search_space_updates()
     )
-
-    api.set_pipeline_config(device="cuda")
-
     api.search(
-        dataset=datamanager,
+        X_train=X_train,
+        y_train=y_train_scaled,
+        X_test=X_test.copy(),
+        y_test=y_test_scaled.copy(),
         optimize_metric='r2',
-        traditional_per_total_budget=0,
         total_walltime_limit=500,
-        func_eval_time_limit=150
+        func_eval_time_limit=50,
+        traditional_per_total_budget=0
     )
 
     ############################################################################
