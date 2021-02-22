89761515f6eb76cbd119aba2f0899857d4a978f8
Arlind Kadra <arlindkadra@gmail.com>
2021-02-22 18:37:23 +0100

    Try at a fix

--- autoPyTorch/pipeline/components/setup/network_backbone/ShapedResNetBackbone.py
+++ autoPyTorch/pipeline/components/setup/network_backbone/ShapedResNetBackbone.py
@@ -158,8 +158,8 @@ class ShapedResNetBackbone(ResNetBackbone):
         cs.add_condition(CS.EqualsCondition(mb_choice, use_sc, True))
         cs.add_condition(
             CS.AndConjunction(
-                CS.EqualsCondition(shake_drop_prob, mb_choice, 'shake-drop'),
-                CS.EqualsCondition(shake_drop_prob, use_sc, True)
+                CS.EqualsCondition(shake_drop_prob, use_sc, True),
+                CS.EqualsCondition(shake_drop_prob, mb_choice, "shake-drop"),
             )
         )
 
