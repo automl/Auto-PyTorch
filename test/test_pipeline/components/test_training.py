5299cbd6bb8d3ff0e988474c47d3c091e54cfbc2
Arlind Kadra <arlindkadra@gmail.com>
2021-02-23 00:33:29 +0100

    Removing duplicate unit test

--- test/test_pipeline/components/test_training.py
+++ test/test_pipeline/components/test_training.py
@@ -446,39 +446,6 @@ class TrainerTest(unittest.TestCase):
                 param2 = params_set[name]
                 self.assertEqual(param1, param2)
 
-    def test_get_set_config_space(self):
-        """Make sure that we can setup a valid choice in the trainer
-        choice"""
-        trainer_choice = TrainerChoice(dataset_properties={'task_type': 'tabular_classification'})
-        cs = trainer_choice.get_hyperparameter_search_space()
-
-        # Make sure that all hyperparameters are part of the serach space
-        self.assertListEqual(
-            sorted(cs.get_hyperparameter('__choice__').choices),
-            sorted(list(trainer_choice.get_components().keys()))
-        )
-
-        # Make sure we can properly set some random configs
-        # Whereas just one iteration will make sure the algorithm works,
-        # doing five iterations increase the confidence. We will be able to
-        # catch component specific crashes
-        for i in range(5):
-            config = cs.sample_configuration()
-            config_dict = copy.deepcopy(config.get_dictionary())
-            trainer_choice.set_hyperparameters(config)
-
-            self.assertEqual(trainer_choice.choice.__class__,
-                             trainer_choice.get_components()[config_dict['__choice__']])
-
-            # Then check the choice configuration
-            selected_choice = config_dict.pop('__choice__', None)
-            for key, value in config_dict.items():
-                # Remove the selected_choice string from the parameter
-                # so we can query in the object for it
-                key = key.replace(selected_choice + ':', '')
-                self.assertIn(key, vars(trainer_choice.choice))
-                self.assertEqual(value, trainer_choice.choice.__dict__[key])
-
 
 if __name__ == '__main__':
     unittest.main()
