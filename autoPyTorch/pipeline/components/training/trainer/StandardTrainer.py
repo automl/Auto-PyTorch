f3d5099940176d13d5d4f766338eae00d4a39edb
Ravin Kohli <kohliravin7@gmail.com>
2021-02-22 14:36:09 +0100

    lookahead in progress

--- autoPyTorch/pipeline/components/training/trainer/StandardTrainer.py
+++ autoPyTorch/pipeline/components/training/trainer/StandardTrainer.py
@@ -1,11 +1,7 @@
-import typing
-
-from ConfigSpace.configuration_space import ConfigurationSpace
-from ConfigSpace.hyperparameters import CategoricalHyperparameter
+from typing import Any, Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
 
-from autoPyTorch.constants import CLASSIFICATION_TASKS, STRING_TO_TASK_TYPES
 from autoPyTorch.pipeline.components.training.trainer.base_trainer import BaseTrainerComponent
 
 
@@ -14,7 +10,7 @@ class StandardTrainer(BaseTrainerComponent):
                  use_swa: bool = False,
                  use_se: bool = False,
                  se_lastk: int = 3,
-                 random_state: typing.Optional[np.random.RandomState] = None):
+                 random_state: Optional[np.random.RandomState] = None):
         """
         This class handles the training of a network for a single given epoch.
 
@@ -29,7 +25,7 @@ class StandardTrainer(BaseTrainerComponent):
                          se_lastk=se_lastk)
 
     def data_preparation(self, X: np.ndarray, y: np.ndarray,
-                         ) -> typing.Tuple[np.ndarray, typing.Dict[str, np.ndarray]]:
+                         ) -> Tuple[np.ndarray, Dict[str, np.ndarray]]:
         """
         Depending on the trainer choice, data fed to the network might be pre-processed
         on a different way. That is, in standard training we provide the data to the
@@ -42,17 +38,17 @@ class StandardTrainer(BaseTrainerComponent):
 
         Returns:
             np.ndarray: that processes data
-            typing.Dict[str, np.ndarray]: arguments to the criterion function
+            Dict[str, np.ndarray]: arguments to the criterion function
         """
         return X, {'y_a': y}
 
     def criterion_preparation(self, y_a: np.ndarray, y_b: np.ndarray = None, lam: float = 1.0
-                              ) -> typing.Callable:
+                              ) -> Callable:
         return lambda criterion, pred: criterion(pred, y_a)
 
     @staticmethod
-    def get_properties(dataset_properties: typing.Optional[typing.Dict[str, typing.Any]] = None
-                       ) -> typing.Dict[str, typing.Union[str, bool]]:
+    def get_properties(dataset_properties: Optional[Dict[str, Any]] = None
+                   ) -> Dict[str, Union[str, bool]]:
         return {
             'shortname': 'StandardTrainer',
             'name': 'StandardTrainer',
@@ -60,20 +56,3 @@ class StandardTrainer(BaseTrainerComponent):
             'handles_image': True,
             'handles_time_series': True,
         }
-
-    @staticmethod
-    def get_hyperparameter_search_space(dataset_properties: typing.Optional[typing.Dict] = None,
-                                        weighted_loss: typing.Tuple[typing.Tuple, bool] = ((True, False), True),
-                                        use_swa: typing.Tuple[typing.Tuple, bool] = ((True, False), True),
-                                        use_se: typing.Tuple[typing.Tuple, bool] = ((True, False), True),
-                                        se_lastk: typing.Tuple[typing.Tuple, int] = ((3,), 3)
-                                        ) -> ConfigurationSpace:
-        cs = ConfigurationSpace()
-        if dataset_properties is not None:
-            if STRING_TO_TASK_TYPES[dataset_properties['task_type']] in CLASSIFICATION_TASKS:
-                weighted_loss = CategoricalHyperparameter("weighted_loss",
-                                                          choices=weighted_loss[0],
-                                                          default_value=weighted_loss[1])
-                cs.add_hyperparameter(weighted_loss)
-
-        return cs
