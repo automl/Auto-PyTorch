01f3b040280eafed43d35108e3b12d077a287dcb
Ravin Kohli <kohliravin7@gmail.com>
2021-02-22 13:40:15 +0100

    ADD tests for SWA, SE
    Add pytest_mock to test dependencies
    
    lookahead in progress
    
    add lookahead hyperparameters

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setuptools.setup(
             "pre-commit",
             "pytest-cov",
             'pytest-forked',
+            "pytest-mock"
             "codecov",
             "pep8",
             "mypy",
