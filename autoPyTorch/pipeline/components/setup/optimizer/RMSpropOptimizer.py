017951c9586b4f3687beaa0d0ef78949e1485572
Arlind Kadra <arlindkadra@gmail.com>
2021-02-22 09:45:15 +0100

    Bug fix

--- autoPyTorch/pipeline/components/setup/optimizer/RMSpropOptimizer.py
+++ autoPyTorch/pipeline/components/setup/optimizer/RMSpropOptimizer.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, Optional, Tuple
 
+import ConfigSpace as CS
 from ConfigSpace.configuration_space import ConfigurationSpace
 from ConfigSpace.hyperparameters import (
     CategoricalHyperparameter,
@@ -112,7 +113,7 @@ class RMSpropOptimizer(BaseOptimizerComponent):
         cs.add_hyperparameters([lr, alpha, weight_decay, momentum, use_wd])
 
         cs.add_condition(
-            ConfigurationSpace.EqualsCondition(
+            CS.EqualsCondition(
                 weight_decay,
                 use_wd,
                 True,
