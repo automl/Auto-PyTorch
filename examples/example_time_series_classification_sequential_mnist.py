2fc058c13d9e59a7dfaf576c79047a63abbb1cca
Sebastian Walter <sebastian.walter98@gmail.com>
2021-02-26 20:19:13 +0100

    added sequential mnist example

--- examples/example_time_series_classification_sequential_mnist.py
+++ examples/example_time_series_classification_sequential_mnist.py
@@ -1,6 +1,6 @@
 """
 ======================
-Time Series Classification
+Time Series Classification on Sequential MNIST
 ======================
 
 The following example shows how to fit a sample classification model
@@ -9,6 +9,11 @@ with AutoPyTorch
 import os
 import tempfile as tmp
 import warnings
+
+import torch
+from torch.utils.data import Subset
+
+import torchvision
 
 os.environ['JOBLIB_TEMP_FOLDER'] = tmp.gettempdir()
 os.environ['OMP_NUM_THREADS'] = '1'
@@ -19,10 +24,6 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import numpy as np
-
-import sklearn.model_selection
-
-from sktime.datasets import load_gunpoint
 
 from autoPyTorch.api.time_series_classification import TimeSeriesClassificationTask
 from autoPyTorch.utils.hyperparameter_search_space_update import HyperparameterSearchSpaceUpdates
@@ -54,43 +55,45 @@ if __name__ == '__main__':
     ############################################################################
     # Data Loading
     # ============
-    X, y = load_gunpoint(return_X_y=True)
+    train_dataset = torchvision.datasets.MNIST(root=".", train=True, download=True)
+    test_dataset = torchvision.datasets.MNIST(root=".", train=False)
 
-    # Convert the pandas dataframes returned from load_gunpoint to 3D numpy array since that is
-    # the format AutoPyTorch expects for now
-    X = [X.iloc[i][0].values for i in range(len(X))]
-    y = [int(y.iloc[i]) for i in range(len(y))]
-    X = np.vstack(X)
+    train_dataset = Subset(train_dataset, indices=torch.randperm(len(train_dataset))[:10000])
+    test_dataset = Subset(train_dataset, indices=torch.randperm(len(test_dataset))[:100])
 
-    # Expand the last dimension because time series data has to be of shape [B, T, F]
-    # where B is the batch size, T is the time dimension and F are the number of features per time step
-    X = X[..., np.newaxis]
+    X_train = np.empty((len(train_dataset), 28 * 28, 1), dtype=np.float32)
+    y_train = np.empty(len(train_dataset), dtype=np.int32)
+    X_test = np.empty((len(test_dataset), 28 * 28, 1), dtype=np.float32)
+    y_test = np.empty(len(test_dataset), dtype=np.int32)
 
-    # Subtract one from the labels because they are initially in {1, 2}, but are expected to be in {0, 1}
-    y = np.array(y) - 1
+    for i, (image, label) in enumerate(train_dataset):
+        X_train[i] = np.asarray(image).reshape(28 * 28, 1)
+        y_train[i] = label
 
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
-        X,
-        y,
-        random_state=1,
-        stratify=y
-    )
+    for i, (image, label) in enumerate(test_dataset):
+        X_test[i] = np.asarray(image).reshape(28 * 28, 1)
+        y_test[i] = label
 
     ############################################################################
     # Build and fit a classifier
     # ==========================
     api = TimeSeriesClassificationTask(
+        n_jobs=4,
         delete_tmp_folder_after_terminate=False,
-        search_space_updates=get_search_space_updates()
+        search_space_updates=get_search_space_updates(),
+        exclude_components={"network_backbone": ["LSTMBackbone"]}
     )
+    api.set_pipeline_config(device="cuda")
     api.search(
         X_train=X_train,
         y_train=y_train,
         X_test=X_test.copy(),
         y_test=y_test.copy(),
+        budget_type="epochs",
+        budget=5,
         optimize_metric='accuracy',
-        total_walltime_limit=500,
-        func_eval_time_limit=50
+        total_walltime_limit=3600,
+        func_eval_time_limit=3600
     )
 
     ############################################################################
