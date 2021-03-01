6b7c2bc1a5d7d22592a18de779d40432902c7473
Ravin Kohli <kohliravin7@gmail.com>
2021-03-01 12:44:54 +0100

    Added n_restarts as hyperparameter for CosineAnnealing

--- autoPyTorch/pipeline/components/setup/lr_scheduler/CosineAnnealingWarmRestarts.py
+++ autoPyTorch/pipeline/components/setup/lr_scheduler/CosineAnnealingWarmRestarts.py
@@ -1,7 +1,8 @@
+import math
 from typing import Any, Dict, Optional, Tuple, Union
 
 from ConfigSpace.configuration_space import ConfigurationSpace
-from ConfigSpace.hyperparameters import UniformFloatHyperparameter, UniformIntegerHyperparameter
+from ConfigSpace.hyperparameters import UniformIntegerHyperparameter
 
 import numpy as np
 
@@ -27,13 +28,11 @@ class CosineAnnealingWarmRestarts(BaseLRComponent):
 
     def __init__(
         self,
-        T_0: int,
-        T_mult: int,
+        n_restarts: int,
         random_state: Optional[np.random.RandomState] = None
     ):
         super().__init__()
-        self.T_0 = T_0
-        self.T_mult = T_mult
+        self.n_restarts = n_restarts
         self.random_state = random_state
         self.scheduler = None  # type: Optional[_LRScheduler]
 
@@ -52,10 +51,14 @@ class CosineAnnealingWarmRestarts(BaseLRComponent):
         # Make sure there is an optimizer
         self.check_requirements(X, y)
 
+        # initialise required attributes for the scheduler
+        T_mult: int = 1
+        T_0: int = math.floor(X['epochs'] / self.n_restarts)
+
         self.scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
             optimizer=X['optimizer'],
-            T_0=int(self.T_0),
-            T_mult=int(self.T_mult),
+            T_0=T_0,
+            T_mult=T_mult,
         )
         return self
 
@@ -70,17 +73,12 @@ class CosineAnnealingWarmRestarts(BaseLRComponent):
     @staticmethod
     def get_hyperparameter_search_space(
         dataset_properties: Optional[Dict] = None,
-        T_0: HyperparameterSearchSpace = HyperparameterSearchSpace(hyperparameter='T_0',
-                                                                   value_range=(1, 20),
-                                                                   default_value=1,
-                                                                   ),
-        T_mult: HyperparameterSearchSpace = HyperparameterSearchSpace(hyperparameter='T_mult',
-                                                                      value_range=(1.0, 2.0),
-                                                                      default_value=1.0,
-                                                                      ),
+        n_restarts: HyperparameterSearchSpace = HyperparameterSearchSpace(hyperparameter='n_restarts',
+                                                                          value_range=(1, 6),
+                                                                          default_value=3,
+                                                                          ),
     ) -> ConfigurationSpace:
         cs = ConfigurationSpace()
-        add_hyperparameter(cs, T_0, UniformIntegerHyperparameter)
-        add_hyperparameter(cs, T_mult, UniformFloatHyperparameter)
+        add_hyperparameter(cs, n_restarts, UniformIntegerHyperparameter)
 
         return cs
