2fc4048ab5800b4cd5df2cd5c83978e08fbfc939
Ravin Kohli <kohliravin7@gmail.com>
2021-03-01 15:52:45 +0100

    cont...

--- autoPyTorch/pipeline/components/training/trainer/base_trainer.py
+++ autoPyTorch/pipeline/components/training/trainer/base_trainer.py
@@ -300,6 +300,7 @@ class BaseTrainerComponent(autoPyTorchTrainingComponent):
                     assert self.swa_model is not None, "SWA model can't be none when" \
                                                        " stochastic weight averaging is enabled"
                     self.swa_model.update_parameters(self.model)
+                    self.swa_updated = True
                 if self.use_snapshot_ensemble:
                     assert self.model_snapshots is not None, "model snapshots container can't be " \
                                                              "none when snapshot ensembling is enabled"
@@ -315,6 +316,7 @@ class BaseTrainerComponent(autoPyTorchTrainingComponent):
                     assert self.swa_model is not None, "SWA model can't be none when" \
                                                        " stochastic weight averaging is enabled"
                     self.swa_model.update_parameters(self.model)
+                    self.swa_updated = True
                 if self.use_snapshot_ensemble:
                     assert self.model_snapshots is not None, "model snapshots container can't be " \
                                                              "none when snapshot ensembling is enabled"
