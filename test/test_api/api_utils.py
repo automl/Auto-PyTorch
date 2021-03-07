0ea47d3944da3ab12a2ed50506d9be16269da221
Ravin Kohli <kohliravin7@gmail.com>
2021-03-07 15:56:41 +0100

    Addressed comments from arlind, change in T_mul and T_0 calculations
    Added debug information for API
    
    Fix flake
    
    Fix import
    
    made test deterministic for feature preprocessing
    
    Fix bug in parsing log
    
    convert to int
    
    Fix bug in testing

---
+++ test/test_api/api_utils.py
@@ -0,0 +1,42 @@
+import glob
+import os
+
+
+def print_debug_information(automl):
+
+    # Log file path
+    log_file = glob.glob(os.path.join(
+        automl._backend.temporary_directory, 'AutoPyTorch*.log'))[0]
+
+    include_messages = ['INFO', 'DEBUG', 'WARN',
+                        'CRITICAL', 'ERROR', 'FATAL']
+
+    # There is a lot of content in the log files. Only
+    # parsing the main message and ignore the metalearning
+    # messages
+    try:
+        with open(log_file) as logfile:
+            content = logfile.readlines()
+
+        # Get the messages to debug easier!
+        content = [line for line in content if any(
+            msg in line for msg in include_messages
+        ) and 'metalearning' not in line]
+
+    except Exception as e:
+        return str(e)
+
+    # Also add the run history if any
+    if hasattr(automl, 'runhistory') and hasattr(automl.runhistory, 'data'):
+        for k, v in automl.runhistory_.data.items():
+            content += ["{}->{}".format(k, v)]
+    else:
+        content += ['No RunHistory']
+
+    # Also add the ensemble history if any
+    if len(automl.ensemble_performance_history) > 0:
+        content += [str(h) for h in automl.ensemble_performance_history]
+    else:
+        content += ['No Ensemble History']
+
+    return os.linesep.join(content)
