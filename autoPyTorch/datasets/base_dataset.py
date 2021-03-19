594bfaab22a6dee98c92a63d3c7f5f7d7bc510a7
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-03-20 05:38:48 +0900

    [refactor] Adress the francisco's comments

--- autoPyTorch/datasets/base_dataset.py
+++ autoPyTorch/datasets/base_dataset.py
@@ -188,10 +188,8 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
             A transformed single point prediction
         """
 
-        if hasattr(self.train_tensors[0], 'loc'):
-            X = self.train_tensors[0].iloc[[index]]
-        else:
-            X = self.train_tensors[0][index]
+        X = self.train_tensors[0].iloc[[index]] if hasattr(self.train_tensors[0], 'loc') \
+            else self.train_tensors[0][index]
 
         if self.train_transform is not None and train:
             X = self.train_transform(X)
