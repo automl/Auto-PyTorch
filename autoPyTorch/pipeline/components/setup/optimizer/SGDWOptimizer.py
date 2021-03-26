102edc4d6511aada63b9163e7020267269dc4014
ArlindKadra <arlindkadra@gmail.com>
2021-03-26 19:59:38 +0100

    Adding missing import

--- autoPyTorch/pipeline/components/setup/optimizer/SGDWOptimizer.py
+++ autoPyTorch/pipeline/components/setup/optimizer/SGDWOptimizer.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Iterable, Optional, Tuple
+from typing import Any, Callable, Dict, Iterable, Optional, Tuple
 
 from ConfigSpace.configuration_space import ConfigurationSpace
 from ConfigSpace.hyperparameters import (
