2d0bc1e215e4b42087244e588e887727eb22c407
Ravin Kohli <kohliravin7@gmail.com>
2021-06-09 11:53:22 +0200

    Fix unit test bug

--- autoPyTorch/pipeline/components/setup/network_backbone/ShapedResNetBackbone.py
+++ autoPyTorch/pipeline/components/setup/network_backbone/ShapedResNetBackbone.py
@@ -38,9 +38,13 @@ class ShapedResNetBackbone(ResNetBackbone):
         self.config.update(
             {"num_units_%d" % (i): num for i, num in enumerate(neuron_counts)}
         )
+        # we are skipping the last layer, as the function get_shaped_neuron_counts
+        # is built for getting neuron counts, so it will add the out_features to
+        # the last layer. However, in dropout we dont want to have that, we just
+        # want to use the shape and not worry about the output.
         if self.config['use_dropout']:
             dropout_shape = get_shaped_neuron_counts(
-                self.config['resnet_shape'], 0, 0, 1000, self.config['num_groups']
+                self.config['resnet_shape'], 0, 0, 1000, self.config['num_groups'] + 1
             )[:-1]
 
             dropout_shape = [
