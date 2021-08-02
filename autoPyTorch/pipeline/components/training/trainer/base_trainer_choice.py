03c260ee15eee3616eb523b1af19ed20f27344aa
Ravin Kohli <kohliravin7@gmail.com>
2021-08-02 12:27:44 +0200

    revert cross entropy weighting

--- autoPyTorch/pipeline/components/training/trainer/base_trainer_choice.py
+++ autoPyTorch/pipeline/components/training/trainer/base_trainer_choice.py
@@ -349,7 +349,7 @@ class TrainerChoice(autoPyTorchChoice):
             trainable_parameter_count,
         )
 
-        self.logger.debug(f"transforms :{X['preprocess_transforms']}")
+        # self.logger.debug(f"transforms :{X['preprocess_transforms']}")
         epoch = 1
 
         while True:
