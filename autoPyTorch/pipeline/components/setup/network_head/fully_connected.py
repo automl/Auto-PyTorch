50d35fcf8f2b4d59c85a9de17d9dfb101ebff16e
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-09-13 11:06:06 +0200

    [fix] Fix a mypy issue

--- autoPyTorch/pipeline/components/setup/network_head/fully_connected.py
+++ autoPyTorch/pipeline/components/setup/network_head/fully_connected.py
@@ -61,7 +61,7 @@ class FullyConnectedHead(NetworkHeadComponent):
         #  changes were made to this module because of failing updates.
         num_layers_is_constant = True if len(num_layers.value_range) == 1 else False
         min_num_layers: int = num_layers.value_range[0]  # type: ignore
-        max_num_layers = num_layers.value_range[-1]  # type: ignore
+        max_num_layers: int = num_layers.value_range[-1]  # type: ignore
 
         num_layers_hp = get_hyperparameter(num_layers, UniformIntegerHyperparameter)
         activation_hp = get_hyperparameter(activation, CategoricalHyperparameter)
