517ccf146da30b00c1fa18048b7af27fc84a85d1
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-11-25 05:39:27 +0900

    [refactor] Merge extraction from history to the result manager
    Since this feature was added in a previous PR, we now rely on this
    feature to extract the history.
    To handle the order by the start time issue, I added the sort by endtime
    feature.

--- autoPyTorch/api/results_manager.py
+++ autoPyTorch/api/results_manager.py
@@ -1,5 +1,6 @@
 import io
-from typing import Any, Dict, List, Optional, Tuple, Union
+from datetime import datetime
+from typing import Any, Dict, List, Tuple, Union
 
 from ConfigSpace.configuration_space import Configuration
 
@@ -11,6 +12,7 @@ from smac.runhistory.runhistory import RunHistory, RunValue
 from smac.tae import StatusType
 from smac.utils.io.traj_logging import TrajEntry
 
+from autoPyTorch import metrics
 from autoPyTorch.pipeline.components.training.metrics.base import autoPyTorchMetric
 
 
@@ -38,6 +40,32 @@ def cost2metric(cost: float, metric: autoPyTorchMetric) -> float:
     return metric._sign * (metric._optimum - cost)
 
 
+def get_start_time(run_history: RunHistory) -> float:
+    """
+    Get start time of optimization.
+
+    Args:
+        run_history (RunHistory):
+            The history of config evals from SMAC.
+
+    Returns:
+        starttime (float):
+            The start time of the first training.
+    """
+
+    start_times = []
+    for run_value in run_history.data.values():
+        status_msg = STATUS2MSG.get(run_value.status, None)
+        if run_value.status in (StatusType.STOP, StatusType.RUNNING):
+            continue
+        elif status_msg is None:
+            raise ValueError(f'Unexpected run status: {run_value.status}')
+
+        start_times.append(run_value.starttime)
+
+    return np.min(start_times)
+
+
 def _extract_metrics_info(
     run_value: RunValue,
     scoring_functions: List[autoPyTorchMetric]
@@ -55,16 +83,97 @@ def _extract_metrics_info(
 
     if run_value.status not in (StatusType.SUCCESS, StatusType.DONOTADVANCE):
         # Additional info for metrics is not available in this case.
-        return {metric.name: np.nan for metric in scoring_functions}
+        return {metric.name: metric._worst_possible_result for metric in scoring_functions}
 
     cost_info = run_value.additional_info['opt_loss']
     avail_metrics = cost_info.keys()
 
     return {
         metric.name: cost2metric(cost=cost_info[metric.name], metric=metric)
-        if metric.name in avail_metrics else np.nan
+        if metric.name in avail_metrics else metric._worst_possible_result
         for metric in scoring_functions
     }
+
+
+class EnsembleResults:
+    def __init__(
+        self,
+        metric: autoPyTorchMetric,
+        ensemble_performance_history: List[Dict[str, Any]],
+        order_by_endtime: bool = False
+    ):
+        self._test_scores: List[float] = []
+        self._train_scores: List[float] = []
+        self._end_times: List[float] = []
+        self._metric = metric
+
+        self._extract_results_from_ensemble_performance_history(ensemble_performance_history)
+        if order_by_endtime:
+            self.sort_by_endtime()
+
+    @property
+    def train_scores(self) -> np.ndarray:
+        return np.asarray(self._train_scores)
+
+    @property
+    def test_scores(self) -> np.ndarray:
+        return np.asarray(self._test_scores)
+
+    @property
+    def end_times(self) -> np.ndarray:
+        return np.asarray(self._end_times)
+
+    def update(self, train_score: float, test_score: float, end_time: float) -> None:
+        self._train_scores.append(train_score)
+        self._test_scores.append(test_score)
+        self._end_times.append(end_time)
+
+    def clear(self) -> None:
+        self._test_scores = []
+        self._train_scores = []
+        self._end_times = []
+
+    def sort_by_endtime(self) -> None:
+        """
+        Since the default order is by start time
+        and parallel computation might change the order of ending,
+        this method provides the feature to sort by end time.
+        Note that this method is destructive.
+        """
+        order = np.argsort(self._end_times)
+
+        self._train_scores = self.train_scores[order].tolist()
+        self._test_scores = self.test_scores[order].tolist()
+        self._end_times = self.end_times[order].tolist()
+
+    def _extract_results_from_ensemble_performance_history(
+        self,
+        ensemble_performance_history: List[Dict[str, Any]]
+    ) -> None:
+        """
+        Extract the information to match this class format.
+
+        Args:
+            ensemble_performance_history (List[Dict[str, Any]]):
+                The history of the ensemble optimization from SMAC.
+                Its keys are `train_xxx`, `test_xxx` or `Timestamp`.
+        """
+
+        self.clear()  # Delete cache before the extraction
+
+        if f'train_{self._metric.name}' not in ensemble_performance_history[0].keys():
+            metric_name = '_'.join([
+                key for key in ensemble_performance_history[0].keys() if key.startswith('train')
+            ][0].split('_')[1:])
+
+            raise KeyError(f'metric_name must be {metric_name}, but got {self._metric.name}')
+
+        for data in ensemble_performance_history:
+            self.update(
+                train_score=data[f'train_{self._metric.name}'],
+                test_score=data[f'test_{self._metric.name}'],
+                end_time=datetime.timestamp(data['Timestamp'])
+            )
 
 
 class SearchResults:
@@ -72,7 +181,8 @@ class SearchResults:
         self,
         metric: autoPyTorchMetric,
         scoring_functions: List[autoPyTorchMetric],
-        run_history: RunHistory
+        run_history: RunHistory,
+        order_by_endtime: bool = False
     ):
         self.metric_dict: Dict[str, List[float]] = {
             metric.name: []
@@ -80,17 +190,20 @@ class SearchResults:
         }
         self._opt_scores: List[float] = []
         self._fit_times: List[float] = []
+        self._end_times: List[float] = []
         self.configs: List[Configuration] = []
         self.status_types: List[str] = []
         self.budgets: List[float] = []
         self.config_ids: List[int] = []
         self.is_traditionals: List[bool] = []
-        self.additional_infos: List[Optional[Dict[str, Any]]] = []
+        self.additional_infos: List[Dict[str, float]] = []
         self.rank_test_scores: np.ndarray = np.array([])
         self._scoring_functions = scoring_functions
         self._metric = metric
 
         self._extract_results_from_run_history(run_history)
+        if order_by_endtime:
+            self.sort_by_endtime()
 
     @property
     def opt_scores(self) -> np.ndarray:
@@ -100,15 +213,20 @@ class SearchResults:
     def fit_times(self) -> np.ndarray:
         return np.asarray(self._fit_times)
 
+    @property
+    def end_times(self) -> np.ndarray:
+        return np.asarray(self._end_times)
+
     def update(
         self,
         config: Configuration,
         status: str,
         budget: float,
         fit_time: float,
+        end_time: float,
         config_id: int,
         is_traditional: bool,
-        additional_info: Dict[str, Any],
+        additional_info: Dict[str, float],
         score: float,
         metric_info: Dict[str, float]
     ) -> None:
@@ -119,6 +237,7 @@ class SearchResults:
         self.config_ids.append(config_id)
         self.is_traditionals.append(is_traditional)
         self.additional_infos.append(additional_info)
+        self._end_times.append(end_time)
         self._fit_times.append(fit_time)
         self._opt_scores.append(score)
 
@@ -128,6 +247,7 @@ class SearchResults:
     def clear(self) -> None:
         self._opt_scores = []
         self._fit_times = []
+        self._end_times = []
         self.configs = []
         self.status_types = []
         self.budgets = []
@@ -135,6 +255,28 @@ class SearchResults:
         self.additional_infos = []
         self.is_traditionals = []
         self.rank_test_scores = np.array([])
+
+    def sort_by_endtime(self) -> None:
+        """
+        Since the default order is by start time
+        and parallel computation might change the order of ending,
+        this method provides the feature to sort by end time.
+        Note that this method is destructive.
+        """
+        order = np.argsort(self._end_times)
+
+        self._opt_scores = np.array(self._opt_scores)[order].tolist()
+        self._fit_times = np.array(self._fit_times)[order].tolist()
+        self._end_times = np.array(self._end_times)[order].tolist()
+        self.configs = np.array(self.configs)[order].tolist()
+        self.status_types = np.array(self.status_types)[order].tolist()
+        self.budgets = np.array(self.budgets)[order].tolist()
+        self.config_ids = np.array(self.config_ids)[order].tolist()
+        self.is_traditionals = np.array(self.is_traditionals)[order].tolist()
+        self.additional_infos = np.array(self.additional_infos)[order].tolist()
+
+        # Only rank_test_scores is np.ndarray
+        self.rank_test_scores = self.rank_test_scores[order]
 
     def _extract_results_from_run_history(self, run_history: RunHistory) -> None:
         """
@@ -166,6 +308,7 @@ class SearchResults:
                 config=config,
                 budget=run_key.budget,
                 fit_time=run_value.time,
+                end_time=run_value.endtime,
                 score=cost2metric(cost=run_value.cost, metric=self._metric),
                 metric_info=_extract_metrics_info(run_value=run_value, scoring_functions=self._scoring_functions),
                 is_traditional=is_traditional,
@@ -179,9 +322,58 @@ class SearchResults:
         )
 
 
+class MetricResults:
+    def __init__(
+        self,
+        metric: autoPyTorchMetric,
+        run_history: RunHistory,
+        ensemble_performance_history: List[Dict[str, Any]]
+    ):
+        self.start_time = get_start_time(run_history)
+        self.metric = metric
+        self.search_results = SearchResults(
+            metric=metric,
+            run_history=run_history,
+            scoring_functions=[],
+            order_by_endtime=True
+        )
+        try:
+            self.ensemble_results = EnsembleResults(
+                metric=metric,
+                ensemble_performance_history=ensemble_performance_history,
+                order_by_endtime=True
+            )
+        except KeyError:
+            self.ensemble_results = None
+
+        self.cum_times = self.search_results.end_times - self.start_time
+        self.data: Dict[str, np.ndarray] = {}
+        self._extract_results()
+
+    def _extract_results(self) -> None:
+        metric_name = self.metric.name
+        worst_val = {metric_name: self.metric._worst_possible_result}
+        for inference_name in ['train', 'test', 'opt']:
+
+            self.data[f'single::{inference_name}::{metric_name}'] = np.array([
+                cost2metric(info.get(f'{inference_name}_loss', worst_val)[metric_name], self.metric)
+                for info in self.search_results.additional_infos
+            ])
+
+            if self.ensemble_results is None or inference_name == 'opt':
+                continue
+
+            self.data[f'ensemble::{inference_name}::{metric_name}'] = np.array(
+                getattr(self.ensemble_results, f'{inference_name}_scores')
+            )
+
+
 class ResultsManager:
     def __init__(self, *args: Any, **kwargs: Any):
         """
+        This module is used to gather result information for BaseTask.
+        In other words, this module is supposed to be wrapped by BaseTask.
+
         Attributes:
             run_history (RunHistory):
                 A `SMAC Runshistory <https://automl.github.io/SMAC3/master/apidoc/smac.runhistory.runhistory.html>`_
