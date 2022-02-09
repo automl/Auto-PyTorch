bf264d67e310e7d6c37ed4b7f5355264f716f82b
nabenabe0928 <47781922+nabenabe0928@users.noreply.github.com>
2022-02-09 20:12:12 +0100

    [feat] Add coalescer (#376)
    * [fix] Add check dataset in transform as well for test dataset, which does not require fit
    * [test] Migrate tests from the francisco's PR without modifications
    * [fix] Modify so that tests pass
    * [test] Increase the coverage

--- test/test_pipeline/components/preprocessing/base.py
+++ test/test_pipeline/components/preprocessing/base.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Optional, Tuple
 from autoPyTorch.pipeline.components.base_choice import autoPyTorchChoice
 from autoPyTorch.pipeline.components.preprocessing.tabular_preprocessing.TabularColumnTransformer import \
     TabularColumnTransformer
+from autoPyTorch.pipeline.components.preprocessing.tabular_preprocessing.coalescer import CoalescerChoice
 from autoPyTorch.pipeline.components.preprocessing.tabular_preprocessing.encoding import EncoderChoice
 from autoPyTorch.pipeline.components.preprocessing.tabular_preprocessing.imputation.SimpleImputer import SimpleImputer
 from autoPyTorch.pipeline.components.preprocessing.tabular_preprocessing.scaling import ScalerChoice
@@ -31,6 +32,7 @@ class TabularPipeline(TabularClassificationPipeline):
         steps.extend([
             ("imputer", SimpleImputer()),
             ("variance_threshold", VarianceThreshold()),
+            ("coalescer", CoalescerChoice(default_dataset_properties)),
             ("encoder", EncoderChoice(default_dataset_properties)),
             ("scaler", ScalerChoice(default_dataset_properties)),
             ("tabular_transformer", TabularColumnTransformer()),
