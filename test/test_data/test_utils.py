ed53a1fb29f32e8abf958144292fc6d35a2e7411
Ravin Kohli <kohliravin7@gmail.com>
2022-02-22 18:34:05 +0100

    fix tests

--- test/test_data/test_utils.py
+++ test/test_data/test_utils.py
@@ -17,18 +17,13 @@ from autoPyTorch.utils.common import subsampler
 
 @pytest.mark.parametrize('openmlid', [2, 40984])
 @pytest.mark.parametrize('as_frame', [True, False])
-def test_data_validation_for_classification(openmlid, as_frame, n_samples):
+def test_reduce_dataset_if_too_large(openmlid, as_frame, n_samples):
     X, _ = fetch_openml(data_id=openmlid, return_X_y=True, as_frame=as_frame)
     X = subsampler(data=X, x=range(n_samples))
 
-    X_converted, precision = reduce_dataset_size_if_too_large(X.copy(), memory_allocation=0)
+    X_converted = reduce_dataset_size_if_too_large(X.copy(), memory_allocation=0)
     np.allclose(X, X_converted) if not as_frame else assert_frame_equal(X, X_converted, check_dtype=False)
     assert megabytes(X_converted) < megabytes(X)
-    if as_frame:
-        assert isinstance(precision, dict)
-        assert isinstance(list(precision.values())[0], type)
-    else:
-        assert isinstance(precision, type)
 
 
 def test_validate_dataset_compression_arg():
