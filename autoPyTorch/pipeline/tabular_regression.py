ae959192f82d499462f426081fec662119cb27c0
Ravin Kohli <kohliravin7@gmail.com>
2022-02-28 11:37:49 +0100

    disable cyclic lr

--- autoPyTorch/pipeline/tabular_regression.py
+++ autoPyTorch/pipeline/tabular_regression.py
@@ -238,6 +238,28 @@ class TabularRegressionPipeline(RegressorMixin, BasePipeline):
                                 raise ValueError("Cannot find a legal default configuration")
                             cs.get_hyperparameter('network_embedding:__choice__').default_value = default
 
+        # Disable CyclicLR until todo is completed.
+        if 'lr_scheduler' in self.named_steps.keys() and 'trainer' in self.named_steps.keys():
+            trainers = cs.get_hyperparameter('trainer:__choice__').choices
+            for trainer in trainers:
+                available_schedulers = self.named_steps['lr_scheduler'].get_available_components(
+                    dataset_properties=dataset_properties,
+                    exclude=exclude if bool(exclude) else None,
+                    include=include if bool(include) else None)
+                # TODO: update cyclic lr to use n_restarts and adjust according to batch size
+                cyclic_lr_scheduler = available_schedulers['CyclicLR']
+                # disable snapshot ensembles and stochastic weight averaging
+                cs.add_forbidden_clause(ForbiddenAndConjunction(
+                    ForbiddenEqualsClause(cs.get_hyperparameter(
+                        f'trainer:{trainer}:use_snapshot_ensemble'), True),
+                    ForbiddenEqualsClause(cs.get_hyperparameter('lr_scheduler:__choice__'), cyclic_lr_scheduler)
+                ))
+                cs.add_forbidden_clause(ForbiddenAndConjunction(
+                    ForbiddenEqualsClause(cs.get_hyperparameter(
+                        f'trainer:{trainer}:use_stochastic_weight_averaging'), True),
+                    ForbiddenEqualsClause(cs.get_hyperparameter('lr_scheduler:__choice__'), cyclic_lr_scheduler)
+                ))
+
         self.configuration_space = cs
         self.dataset_properties = dataset_properties
         return cs
