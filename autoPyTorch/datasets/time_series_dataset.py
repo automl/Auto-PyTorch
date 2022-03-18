fixed scaler computation

autoPyTorch/datasets/time_series_dataset.py
@@ -196,12 +196,15 @@ class TimeSeriesSequence(Dataset):
         past_target = targets[:index + 1]
         past_target = torch.from_numpy(past_target)
 
+        # TODO combine with imputer!
+        past_observed_values = torch.ones([past_target.shape[0], 1], dtype=torch.bool)
+
         return {"past_targets": past_target,
                 "past_features": past_features,
                 "future_features": future_features,
                 "static_features": self.static_features,
                 "mase_coefficient": self.mase_coefficient,
-                'encoder_lengths': past_target.shape[0],
+                'past_observed_values': past_observed_values,
                 'decoder_lengths': None if targets_future is None else targets_future.shape[0]}, targets_future
 
     def __len__(self) -> int:
@@ -327,7 +330,7 @@ class TimeSeriesForecastingDataset(BaseDataset, ConcatDataset):
                  lagged_value: Optional[List[int]] = None,
                  n_prediction_steps: int = 1,
                  dataset_name: Optional[str] = None,
-                 normalize_y: bool = True,
+                 normalize_y: bool = False,
                  static_features: Optional[np.ndarray] = None,
                  ):
         """
@@ -1051,7 +1054,7 @@ class TimeSeriesForecastingDataset(BaseDataset, ConcatDataset):
         refit_set.splits = refit_set.get_splits_from_resampling_strategy()
         return refit_set
 
-    def generatet_test_seqs(self) -> List[TimeSeriesSequence]:
+    def generate_test_seqs(self) -> List[TimeSeriesSequence]:
         test_sets = copy.deepcopy(self.datasets)
         for test_seq in test_sets:
             test_seq.is_test_set = True

autoPyTorch/pipeline/components/preprocessing/time_series_preprocessing/forecasting_target_scaling/base_target_scaler.py
@@ -54,6 +54,7 @@ class BaseTargetScaler(autoPyTorchTimeSeriesPreprocessingComponent):
 
     def __call__(self,
                  past_target: Union[np.ndarray, torch.tensor],
+                 past_observed_values: Optional[torch.BoolTensor] = None,
                  future_targets: Optional[Union[np.ndarray, torch.Tensor]]=None,
                  ) -> Union[np.ndarray, torch.tensor]:
 
@@ -64,5 +65,7 @@ class BaseTargetScaler(autoPyTorchTimeSeriesPreprocessingComponent):
         if len(past_target.shape) == 2:
             # expand batch dimension when called on a single record
             past_target = past_target[np.newaxis, ...]
-        past_target, future_targets, loc, scale = self.scaler.transform(past_target, future_targets)
+        past_target, future_targets, loc, scale = self.scaler.transform(past_target,
+                                                                        past_observed_values,
+                                                                        future_targets)
         return past_target, future_targets, loc, scale

autoPyTorch/pipeline/components/preprocessing/time_series_preprocessing/forecasting_target_scaling/utils.py
@@ -11,54 +11,116 @@ class TargetScaler(BaseEstimator):
     """
     To accelerate training, this scaler is only applied under trainer (after the data is loaded by dataloader)
     """
+
     def __init__(self, mode: str):
         self.mode = mode
 
     def fit(self, X: Dict, y: Any = None) -> "TimeSeriesScalerBatch":
         return self
 
-    def transform(self, past_targets: torch.Tensor, future_targets: Optional[torch.Tensor]=None) -> \
+    def transform(self,
+                  past_targets: torch.Tensor,
+                  past_observed_values: torch.BoolTensor,
+                  future_targets: Optional[torch.Tensor] = None) -> \
             Tuple[torch.Tensor, Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
-        if self.mode == "standard":
-            loc = torch.mean(past_targets, dim=-2, keepdim=True)
-            scale = torch.std(past_targets, dim=-2, keepdim=True)
+        if past_observed_values is None or torch.all(past_observed_values):
+            if self.mode == "standard":
+                loc = torch.mean(past_targets, dim=1, keepdim=True)
+                scale = torch.std(past_targets, dim=1, keepdim=True)
 
-            scale[scale == 0.0] = 1.0
-            if future_targets is not None:
-                future_targets = (future_targets - loc) / scale
-            return (past_targets - loc) / scale, future_targets, loc, scale
+                scale[scale == 0.0] = 1.0
+                if future_targets is not None:
+                    future_targets = (future_targets - loc) / scale
+                return (past_targets - loc) / scale, future_targets, loc, scale
 
-        elif self.mode == "min_max":
-            min_ = torch.min(past_targets, dim=-2, keepdim=True)[0]
-            max_ = torch.max(past_targets, dim=-2, keepdim=True)[0]
+            elif self.mode == "min_max":
+                min_ = torch.min(past_targets, dim=1, keepdim=True)[0]
+                max_ = torch.max(past_targets, dim=1, keepdim=True)[0]
 
-            diff_ = max_ - min_
-            loc = min_ - 1e-10
-            scale = diff_
-            scale[scale == 0.0] = 1.0
-            if future_targets is not None:
-                future_targets = (future_targets - loc) / scale
-            return (past_targets - loc) / scale, future_targets, loc, scale
+                diff_ = max_ - min_
+                loc = min_ - 1e-10
+                scale = diff_
+                scale[scale == 0.0] = 1.0
+                if future_targets is not None:
+                    future_targets = (future_targets - loc) / scale
+                return (past_targets - loc) / scale, future_targets, loc, scale
 
-        elif self.mode == "max_abs":
-            max_abs_ = torch.max(torch.abs(past_targets), dim=-2, keepdim=True)[0]
-            max_abs_[max_abs_ == 0.0] = 1.0
-            scale = max_abs_
-            if future_targets is not None:
-                future_targets = future_targets / scale
-            return past_targets / scale, future_targets, None, scale
+            elif self.mode == "max_abs":
+                max_abs_ = torch.max(torch.abs(past_targets), dim=1, keepdim=True)[0]
+                max_abs_[max_abs_ == 0.0] = 1.0
+                scale = max_abs_
+                if future_targets is not None:
+                    future_targets = future_targets / scale
+                return past_targets / scale, future_targets, None, scale
 
-        elif self.mode == 'mean_abs':
-            mean_abs = torch.mean(torch.abs(past_targets), dim=1,  keepdim=True)
-            mean_abs[mean_abs == 0.0] = 1.0
-            scale = mean_abs
-            if future_targets is not None:
-                future_targets = future_targets / scale
-            return past_targets / scale, future_targets, None, scale
+            elif self.mode == 'mean_abs':
+                mean_abs = torch.mean(torch.abs(past_targets), dim=1, keepdim=True)
+                mean_abs[mean_abs == 0.0] = 1.0
+                scale = mean_abs
+                if future_targets is not None:
+                    future_targets = future_targets / scale
+                return past_targets / scale, future_targets, None, scale
 
+            elif self.mode == "none":
+                return past_targets, future_targets, None, None
 
-        elif self.mode == "none":
-            return past_targets, future_targets, None, None
-
+            else:
+                raise ValueError(f"Unknown mode {self.mode} for Forecasting scaler")
         else:
-            raise ValueError(f"Unknown mode {self.mode} for Forecasting scaler")
+            valid_past_targets = past_observed_values * past_targets
+            valid_past_obs = torch.sum(past_observed_values, dim=1, keepdim=True)
+            if self.mode == "standard":
+                dfredom = 1
+                loc = torch.sum(valid_past_targets, dim=1, keepdim=True) / valid_past_obs
+                scale = torch.sum(torch.square((valid_past_targets - loc) * past_observed_values), dim=1, keepdim=True)
+                scale /= valid_past_obs - dfredom
+                scale = torch.sqrt(scale)
+
+                offset_targets = past_targets - loc
+                # ensure that all the targets are scaled properly
+                scale = torch.where(torch.logical_or(scale == 0.0, scale == torch.nan), offset_targets[:, [-1]], scale)
+                scale[scale == 0.0] = 1.0
+
+                if future_targets is not None:
+                    future_targets = (future_targets - loc) / scale
+
+                scaled_past_targets = torch.where(past_observed_values, offset_targets / scale, past_targets)
+                return scaled_past_targets, future_targets, loc, scale
+
+            elif self.mode == "min_max":
+                obs_mask = ~past_observed_values
+                min_masked_past_targets = past_targets.masked_fill(obs_mask, value=torch.inf)
+                max_masked_past_targets = past_targets.masked_fill(obs_mask, value=-torch.inf)
+                min_ = torch.min(min_masked_past_targets, dim=1, keepdim=True)[0]
+                max_ = torch.max(max_masked_past_targets, dim=1, keepdim=True)[0]
+
+                diff_ = max_ - min_
+                loc = min_ - 1e-10
+                scale = torch.where(diff_ == 0, past_targets[:, [-1]], diff_)
+                scale[scale == 0.0] = 1.0
+
+                if future_targets is not None:
+                    future_targets = (future_targets - loc) / scale
+                return (past_targets - loc) / scale, future_targets, loc, scale
+
+            elif self.mode == "max_abs":
+                max_abs_ = torch.max(torch.abs(valid_past_targets), dim=1, keepdim=True)[0]
+                scale = torch.where(max_abs_ == 0, past_targets[:, [-1]], max_abs_)
+                if future_targets is not None:
+                    future_targets = future_targets / scale
+                return past_targets / scale, future_targets, None, scale
+
+            elif self.mode == 'mean_abs':
+                mean_abs = torch.sum(torch.abs(valid_past_targets), dim=1, keepdim=True) / valid_past_obs
+                scale = torch.where(mean_abs == 0.0, valid_past_targets[:, [-1]], mean_abs)
+                # in case that all values in the tensor is 0
+                scale[scale == 0.0] = 1.0
+                if future_targets is not None:
+                    future_targets = future_targets / scale
+                return past_targets / scale, future_targets, None, scale
+
+            elif self.mode == "none":
+                return past_targets, future_targets, None, None
+
+            else:
+                raise ValueError(f"Unknown mode {self.mode} for Forecasting scaler")

autoPyTorch/pipeline/components/setup/network/forecasting_architecture.py
@@ -316,7 +316,7 @@ class AbstractForecastingNet(nn.Module):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.Tensor] = None,
+                past_observed_values: Optional[torch.Tensor] = None,
                 decoder_observed_values: Optional[torch.Tensor] = None,
                 ):
         raise NotImplementedError
@@ -352,6 +352,7 @@ class AbstractForecastingNet(nn.Module):
 class ForecastingNet(AbstractForecastingNet):
     def pre_processing(self,
                        past_targets: torch.Tensor,
+                       past_observed_values: torch.BoolTensor,
                        past_features: Optional[torch.Tensor] = None,
                        future_features: Optional[torch.Tensor] = None,
                        static_features: Optional[torch.Tensor] = None,
@@ -360,7 +361,10 @@ class ForecastingNet(AbstractForecastingNet):
                        variable_selector_kwargs: Dict = {},
                        ):
         if self.encoder_lagged_input:
-            past_targets[:, -self.window_size:], _, loc, scale = self.target_scaler(past_targets[:, -self.window_size:])
+            past_targets[:, -self.window_size:], _, loc, scale = self.target_scaler(
+                past_targets[:, -self.window_size:],
+                past_observed_values[:, -self.window_size:]
+            )
             past_targets[:, :-self.window_size] = self.scale_value(past_targets[:, :-self.window_size], loc, scale)
             x_past, self.cached_lag_mask_encoder = get_lagged_subsequences(past_targets,
                                                                            self.window_size,
@@ -369,7 +373,8 @@ class ForecastingNet(AbstractForecastingNet):
         else:
             if self.window_size < past_targets.shape[1]:
                 past_targets = past_targets[:, -self.window_size:]
-            past_targets, _, loc, scale = self.target_scaler(past_targets)
+                past_observed_values = past_observed_values[:, -self.window_size:]
+            past_targets, _, loc, scale = self.target_scaler(past_targets, past_observed_values)
             x_past = past_targets
 
         if self.network_structure.variable_selection:
@@ -440,11 +445,12 @@ class ForecastingNet(AbstractForecastingNet):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.LongTensor] = None,
+                past_observed_values: Optional[torch.BoolTensor] = None,
                 decoder_observed_values: Optional[torch.Tensor] = None,
                 ):
         x_past, x_future, x_static, loc, scale, static_context_initial_hidden = self.pre_processing(
             past_targets=past_targets,
+            past_observed_values=past_observed_values,
             past_features=past_features,
             future_features=future_features,
             static_features=static_features,
@@ -460,11 +466,14 @@ class ForecastingNet(AbstractForecastingNet):
         if self.has_temporal_fusion:
             decoder_output = self.temporal_fusion(encoder_output=encoder_output,
                                                   decoder_output=decoder_output,
-                                                  encoder_lengths=encoder_lengths,
+                                                  past_observed_values=past_observed_values,
                                                   decoder_length=self.n_prediction_steps,
                                                   static_embedding=x_static
                                                   )
+
         output = self.head(decoder_output)
+
+
         return self.rescale_output(output, loc, scale, self.device)
 
     def pred_from_net_output(self, net_output):
@@ -499,13 +508,13 @@ class ForecastingNet(AbstractForecastingNet):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.LongTensor] = None,
+                past_observed_values: Optional[torch.BoolTensor] = None,
                 ):
         net_output = self(past_targets=past_targets,
                           past_features=past_features,
                           future_features=future_features,
                           static_features=static_features,
-                          encoder_lengths=encoder_lengths)
+                          past_observed_values=past_observed_values)
         return self.pred_from_net_output(net_output)
 
 
@@ -558,10 +567,11 @@ class ForecastingSeq2SeqNet(ForecastingNet):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.Tensor] = None,
+                past_observed_values: Optional[torch.Tensor] = None,
                 decoder_observed_values: Optional[torch.Tensor] = None, ):
         x_past, x_future, x_static, loc, scale, static_context_initial_hidden = self.pre_processing(
             past_targets=past_targets,
+            past_observed_values=past_observed_values,
             past_features=past_features,
             future_features=future_features,
             static_features=static_features,
@@ -598,7 +608,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
             if self.has_temporal_fusion:
                 decoder_output = self.temporal_fusion(encoder_output=encoder_output,
                                                       decoder_output=decoder_output,
-                                                      encoder_lengths=encoder_lengths,
+                                                      past_observed_values=past_observed_values,
                                                       decoder_length=self.n_prediction_steps,
                                                       static_embedding=x_static
                                                       )
@@ -649,7 +659,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
                             decoder_output_all = decoder_output
                         decoder_output = self.temporal_fusion(encoder_output=encoder_output,
                                                               decoder_output=decoder_output_all,
-                                                              encoder_lengths=encoder_lengths,
+                                                              past_observed_values=past_observed_values,
                                                               decoder_length=idx_pred + 1,
                                                               static_embedding=x_static
                                                               )[:, -1:]
@@ -679,12 +689,12 @@ class ForecastingSeq2SeqNet(ForecastingNet):
                     is_hidden_states=self.encoder.encoder_has_hidden_states,
                     repeats=self.num_samples)
 
-                intermediate_values = self.repeat_intermediate_values([encoder_output, encoder_lengths],
+                intermediate_values = self.repeat_intermediate_values([encoder_output, past_observed_values],
                                                                       is_hidden_states=[False, False],
                                                                       repeats=self.num_samples)
 
                 encoder_output = intermediate_values[0]
-                encoder_lengths = intermediate_values[1]
+                past_observed_values = intermediate_values[1]
 
                 if self.decoder_lagged_input:
                     max_lag_seq_length = max(self.decoder_lagged_value) + 1
@@ -737,7 +747,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
                             decoder_output_all = decoder_output
                         decoder_output = self.temporal_fusion(encoder_output=encoder_output,
                                                               decoder_output=decoder_output_all,
-                                                              encoder_lengths=encoder_lengths,
+                                                              past_observed_values=past_observed_values,
                                                               decoder_length=idx_pred + 1,
                                                               static_embedding=x_static,
                                                               )[:, -1:]
@@ -764,13 +774,13 @@ class ForecastingSeq2SeqNet(ForecastingNet):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.LongTensor] = None,
+                past_observed_values: Optional[torch.BoolTensor] = None,
                 ):
         net_output = self(past_targets=past_targets,
                           past_features=past_features,
                           future_features=future_features,
                           static_features=static_features,
-                          encoder_lengths=encoder_lengths)
+                          past_observed_values=past_observed_values)
         if self.output_type == 'regression':
             return self.pred_from_net_output(net_output)
         else:
@@ -832,13 +842,17 @@ class ForecastingDeepARNet(ForecastingSeq2SeqNet):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.Tensor] = None,
+                past_observed_values: Optional[torch.Tensor] = None,
                 decoder_observed_values: Optional[torch.Tensor] = None, ):
         if self.training:
             if self.encoder_lagged_input:
                 past_targets[:, -self.window_size:], _, loc, scale = self.target_scaler(
-                    past_targets[:, -self.window_size:])
+                    past_targets[:, -self.window_size:],
+                    past_observed_values[:, -self.window_size:]
+                )
+
                 past_targets[:, :-self.window_size] = self.scale_value(past_targets[:, :-self.window_size], loc, scale)
+
                 future_targets = self.scale_value(future_targets, loc, scale)
 
                 targets_all = torch.cat([past_targets, future_targets[:, :-1]], dim=1)
@@ -850,7 +864,8 @@ class ForecastingDeepARNet(ForecastingSeq2SeqNet):
             else:
                 if self.window_size < past_targets.shape[1]:
                     past_targets = past_targets[:, -self.window_size:]
-                past_targets, _, loc, scale = self.target_scaler(past_targets)
+                    past_observed_values = past_observed_values[:, -self.window_size:]
+                past_targets, _, loc, scale = self.target_scaler(past_targets, past_observed_values)
                 future_targets = self.scale_value(future_targets, loc, scale)
                 targets_all = torch.cat([past_targets, future_targets[:, :-1]], dim=1)
 
@@ -890,8 +905,12 @@ class ForecastingDeepARNet(ForecastingSeq2SeqNet):
         else:
             if self.encoder_lagged_input:
                 past_targets[:, -self.window_size:], _, loc, scale = self.target_scaler(
-                    past_targets[:, -self.window_size:])
+                    past_targets[:, -self.window_size:],
+                    past_observed_values[:, -self.window_size:],
+                )
+
                 past_targets[:, :-self.window_size] = self.scale_value(past_targets[:, :-self.window_size], loc, scale)
+
                 x_past, self.cached_lag_mask_encoder_test = get_lagged_subsequences(past_targets,
                                                                                     self.window_size,
                                                                                     self.encoder_lagged_value,
@@ -899,8 +918,8 @@ class ForecastingDeepARNet(ForecastingSeq2SeqNet):
             else:
                 if self.window_size < past_targets.shape[1]:
                     past_targets = past_targets[:, -self.window_size:]
-
-                past_targets, _, loc, scale = self.target_scaler(past_targets)
+                    past_observed_values = past_observed_values[:, -self.window_size]
+                past_targets, _, loc, scale = self.target_scaler(past_targets, past_observed_values)
                 x_past = past_targets
 
             if self.network_structure.variable_selection:
@@ -1040,13 +1059,13 @@ class ForecastingDeepARNet(ForecastingSeq2SeqNet):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.LongTensor] = None,
+                past_observed_values: Optional[torch.BoolTensor] = None,
                 ):
         net_output = self(past_targets=past_targets,
                           past_features=past_features,
                           future_features=future_features,
                           static_features=static_features,
-                          encoder_lengths=encoder_lengths)
+                          past_observed_values=past_observed_values)
         return net_output
 
 
@@ -1059,11 +1078,12 @@ class NBEATSNet(ForecastingNet):
                 past_features: Optional[torch.Tensor] = None,
                 future_features: Optional[torch.Tensor] = None,
                 static_features: Optional[torch.Tensor] = None,
-                encoder_lengths: Optional[torch.Tensor] = None,
+                past_observed_values: Optional[torch.Tensor] = None,
                 decoder_observed_values: Optional[torch.Tensor] = None, ):
         if self.window_size < past_targets.shape[1]:
             past_targets = past_targets[:, -self.window_size:]
-        past_targets, _, loc, scale = self.target_scaler(past_targets)
+            past_observed_values = past_observed_values[:, -self.window_size]
+        past_targets, _, loc, scale = self.target_scaler(past_targets, past_observed_values)
         past_targets = past_targets.to(self.device)
 
         batch_size = past_targets.shape[0]

autoPyTorch/pipeline/components/setup/network/forecasting_network.py
@@ -148,7 +148,7 @@ class ForecastingNetworkComponent(NetworkComponent):
             past_features = X_batch['past_features']
             future_features = X_batch["future_features"]
             static_features = X_batch["static_features"]
-            encoder_lengths = X_batch['encoder_lengths']
+            past_observed_values = X_batch['past_observed_values']
 
             if past_targets.ndim == 2:
                 past_targets = past_targets.unsqueeze(-1)
@@ -162,7 +162,7 @@ class ForecastingNetworkComponent(NetworkComponent):
                 if pred_kwargs[key] is not None:
                     pred_kwargs[key] = pred_kwargs[key].float()
 
-            pred_kwargs.update({'encoder_lengths': encoder_lengths})
+            pred_kwargs.update({'past_observed_values': past_observed_values})
 
             with torch.no_grad():
                 Y_batch_pred = self.network.predict(**pred_kwargs)

autoPyTorch/pipeline/components/setup/network_backbone/forecasting_backbone/cells.py
@@ -94,14 +94,14 @@ class TemporalFusionLayer(nn.Module):
     def forward(self,
                 encoder_output: torch.Tensor,
                 decoder_output: torch.Tensor,
-                encoder_lengths: torch.LongTensor,
+                past_observed_values: torch.BoolTensor,
                 decoder_length: int,
                 static_embedding: Optional[torch.Tensor] = None):
         """
         Args:
             encoder_output: the output of the last layer of encoder network
             decoder_output: the output of the last layer of decoder network
-            encoder_lengths: length of encoder network
+            past_observed_values: observed values in the past
             decoder_length: length of decoder network
             static_embedding: output of static variable selection network (if applible)
         """
@@ -120,11 +120,10 @@ class TemporalFusionLayer(nn.Module):
 
         # Attention
         encoder_out_length = encoder_output.shape[1]
-        encoder_lengths = torch.where(encoder_lengths < self.window_size, encoder_lengths, self.window_size)
-        encoder_lengths = torch.where(encoder_lengths > encoder_out_length, encoder_out_length, encoder_lengths)
-        encoder_lengths = encoder_lengths.to(self.device)
+        past_observed_values = past_observed_values[:, -encoder_out_length:]
+        past_observed_values = past_observed_values.to(self.device)
 
-        mask = self.get_attention_mask(encoder_lengths=encoder_lengths, decoder_length=decoder_length)
+        mask = self.get_attention_mask(past_observed_values=past_observed_values, decoder_length=decoder_length)
         if mask.shape[-1] < attn_input.shape[1]:
             # in case that none of the samples has length greater than window_size
             mask = torch.cat([
@@ -156,7 +155,7 @@ class TemporalFusionLayer(nn.Module):
         self.to(device)
         self._device = device
 
-    def get_attention_mask(self, encoder_lengths: torch.LongTensor, decoder_length: int):
+    def get_attention_mask(self, past_observed_values: torch.BoolTensor, decoder_length: int):
         """
         https://github.com/jdb78/pytorch-forecasting/blob/master/pytorch_forecasting/models/
         temporal_fusion_transformer/__init__.py
@@ -174,16 +173,14 @@ class TemporalFusionLayer(nn.Module):
         #   data and self
         decoder_mask = attend_step >= predict_step
         # do not attend to steps where data is padded
-        encoder_mask = create_mask(encoder_lengths.max(), encoder_lengths)
-
         # this is the result of our padding strategy: we pad values at the start of the tensors
-        encoder_mask = torch.flip(encoder_mask, dims=[1])
+        encoder_mask = ~past_observed_values.squeeze(-1)
 
         # combine masks along attended time - first encoder and then decoder
         mask = torch.cat(
             (
                 encoder_mask.unsqueeze(1).expand(-1, decoder_length, -1),
-                decoder_mask.unsqueeze(0).expand(encoder_lengths.size(0), -1, -1),
+                decoder_mask.unsqueeze(0).expand(encoder_mask.size(0), -1, -1),
             ),
             dim=2,
         )

autoPyTorch/pipeline/components/training/data_loader/time_series_util.py
@@ -46,8 +46,10 @@ def pad_sequence_with_minimal_length(sequences: List[torch.Tensor],
         out_dims = (len(sequences), max_len) + trailing_dims
     else:
         out_dims = (max_len, len(sequences)) + trailing_dims
-
-    out_tensor = sequences[0].new_full(out_dims, padding_value)
+    if sequences[0].dtype == torch.bool:
+        out_tensor = sequences[0].new_full(out_dims, False)
+    else:
+        out_tensor = sequences[0].new_full(out_dims, padding_value)
     for i, tensor in enumerate(sequences):
         length = min(tensor.size(0), seq_max_length)
         # use index notation to prevent duplicate references to the tensor

autoPyTorch/pipeline/components/training/losses.py
@@ -48,9 +48,14 @@ class MAPELoss(Loss):
         super(MAPELoss, self).__init__(reduction)
 
     def forward(self, input: torch.distributions.Distribution, target_tensor: torch.Tensor) -> torch.Tensor:
-        target = torch.abs(target_tensor)
-        target[target == 0] = 1
-        loss = torch.abs(input - target_tensor) / target
+        # https://github.com/awslabs/gluon-ts/blob/master/src/gluonts/model/n_beats/_network.py
+        denominator = torch.abs(target_tensor)
+        diff = torch.abs(input - target_tensor)
+
+        flag = (denominator == 0).float()
+
+        loss = (diff * (1 - flag)) / (denominator + flag)
+
         if self.reduction == 'mean':
             return loss.mean()
         elif self.reduction == 'sum':
@@ -104,6 +109,7 @@ class QuantileLoss(Loss):
 
             loss_q = torch.max(q * diff, (q - 1) * diff)
             losses_all.append(loss_q.unsqueeze(-1))
+
         losses_all = torch.mean(torch.concat(losses_all, dim=-1), dim=-1)
 
         if self.reduction == 'mean':

autoPyTorch/pipeline/components/training/trainer/forecasting_trainer/forecasting_base_trainer.py
@@ -118,7 +118,6 @@ class ForecastingBaseTrainerComponent(BaseTrainerComponent, ABC):
 
         time_end = time.time()
         print(f'time used epoch {epoch}: {time_end - time_start}')
-        print(f'loss: {loss_sum / N}')
 
         if self.metrics_during_training:
             return loss_sum / N, self.compute_metrics(outputs_data, targets_data)
@@ -149,7 +148,7 @@ class ForecastingBaseTrainerComponent(BaseTrainerComponent, ABC):
             float: the loss incurred in the prediction
         """
         past_target = data['past_targets'].float()
-        encoder_lengths = data['encoder_lengths']
+        past_observed_values = data['past_observed_values']
 
         past_features = data["past_features"]
         if past_features is not None:
@@ -163,7 +162,6 @@ class ForecastingBaseTrainerComponent(BaseTrainerComponent, ABC):
 
         future_targets = self.cast_targets(future_targets)
 
-
         if isinstance(self.criterion, MASELoss):
             self.criterion.set_mase_coefficient(data['mase_coefficient'].float().to(self.device))
 
@@ -172,10 +170,11 @@ class ForecastingBaseTrainerComponent(BaseTrainerComponent, ABC):
 
         if isinstance(self.model, NBEATSNet):
             past_target = past_target[:, -self.window_size:]
+            past_observed_values = past_observed_values[:, -self.window_size:]
             past_target, criterion_kwargs_past = self.data_preparation(past_target,
                                                                        past_target.to(self.device))
             past_target, criterion_kwargs_future = self.data_preparation(past_target, future_targets.to(self.device))
-            backcast, forecast = self.model(past_targets=past_target, encoder_lengths=encoder_lengths)
+            backcast, forecast = self.model(past_targets=past_target, past_observed_values=past_observed_values)
 
             loss_func_backcast = self.criterion_preparation(**criterion_kwargs_past)
             loss_func_forecast = self.criterion_preparation(**criterion_kwargs_future)
@@ -204,7 +203,7 @@ class ForecastingBaseTrainerComponent(BaseTrainerComponent, ABC):
                                  future_features=future_features,
                                  static_features=static_features,
                                  future_targets=future_targets,
-                                 encoder_lengths=encoder_lengths)
+                                 past_observed_values=past_observed_values)
 
             loss_func = self.criterion_preparation(**criterion_kwargs)
 
@@ -248,7 +247,7 @@ class ForecastingBaseTrainerComponent(BaseTrainerComponent, ABC):
         with torch.no_grad():
             for step, (data, future_targets) in enumerate(test_loader):
                 past_target = data['past_targets'].float()
-                encoder_lengths = data['encoder_lengths']
+                past_observed_values = data['past_observed_values']
 
                 past_features = data["past_features"]
                 if past_features is not None:
@@ -276,13 +275,13 @@ class ForecastingBaseTrainerComponent(BaseTrainerComponent, ABC):
                                          future_targets=future_targets,
                                          future_features=future_features,
                                          static_features=static_features,
-                                         encoder_lengths=encoder_lengths)
+                                         past_observed_values=past_observed_values)
                 else:
                     outputs = self.model(past_targets=past_target,
                                          past_features=past_features,
                                          future_features=future_features,
                                          static_features=static_features,
-                                         encoder_lengths=encoder_lengths)
+                                         past_observed_values=past_observed_values)
 
                 # prepare
                 future_targets = future_targets.to(self.device)