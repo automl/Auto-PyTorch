fix uncorrect Seq2Seq scaling

autoPyTorch/pipeline/components/setup/network/forecasting_architecture.py
@@ -428,7 +428,7 @@ class ForecastingNet(AbstractForecastingNet):
                 **variable_selector_kwargs
             )
 
-            return x_past, x_future, x_static, loc, scale, static_context_initial_hidden
+            return x_past, x_future, x_static, loc, scale, static_context_initial_hidden, past_targets
         else:
             if past_features is not None:
                 past_features = past_features[:, -self.window_size:]
@@ -440,7 +440,7 @@ class ForecastingNet(AbstractForecastingNet):
             if static_features is not None:
                 static_features = static_features.to(self.device)
             x_past = self.embedding(x_past)  # TODO embedding for future features!
-            return x_past, future_features, static_features, loc, scale, None
+            return x_past, future_features, static_features, loc, scale, None, past_targets
 
     def forward(self,
                 past_targets: torch.Tensor,
@@ -451,7 +451,7 @@ class ForecastingNet(AbstractForecastingNet):
                 past_observed_values: Optional[torch.BoolTensor] = None,
                 decoder_observed_values: Optional[torch.Tensor] = None,
                 ):
-        x_past, x_future, x_static, loc, scale, static_context_initial_hidden = self.pre_processing(
+        x_past, x_future, x_static, loc, scale, static_context_initial_hidden, _ = self.pre_processing(
             past_targets=past_targets,
             past_observed_values=past_observed_values,
             past_features=past_features,
@@ -464,7 +464,9 @@ class ForecastingNet(AbstractForecastingNet):
         encoder_additional = [static_context_initial_hidden]
         encoder_additional.extend([None] * (self.network_structure.num_blocks - 1))
         encoder2decoder, encoder_output = self.encoder(encoder_input=x_past, additional_input=encoder_additional)
-        decoder_output = self.decoder(x_future=x_future, encoder_output=encoder2decoder)
+
+        decoder_output = self.decoder(x_future=x_future, encoder_output=encoder2decoder,
+                                      pos_idx=(x_past.shape[1], x_past.shape[1] + self.n_prediction_steps))
 
         if self.has_temporal_fusion:
             decoder_output = self.temporal_fusion(encoder_output=encoder_output,
@@ -475,7 +477,6 @@ class ForecastingNet(AbstractForecastingNet):
                                                   )
 
         output = self.head(decoder_output)
-
 
         return self.rescale_output(output, loc, scale, self.device)
 
@@ -572,7 +573,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
                 static_features: Optional[torch.Tensor] = None,
                 past_observed_values: Optional[torch.BoolTensor] = None,
                 decoder_observed_values: Optional[torch.Tensor] = None, ):
-        x_past, x_future, x_static, loc, scale, static_context_initial_hidden = self.pre_processing(
+        x_past, x_future, x_static, loc, scale, static_context_initial_hidden, past_targets = self.pre_processing(
             past_targets=past_targets,
             past_observed_values=past_observed_values,
             past_features=past_features,
@@ -586,6 +587,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
         encoder_additional.extend([None] * (self.network_structure.num_blocks - 1))
 
         if self.training:
+            future_targets = self.scale_value(future_targets, loc, scale)
             # we do one step ahead forecasting
             if self.decoder_lagged_input:
                 future_targets = torch.cat([past_targets, future_targets[:, :-1, :]], dim=1)
@@ -606,7 +608,8 @@ class ForecastingSeq2SeqNet(ForecastingNet):
             encoder2decoder, encoder_output = self.encoder(encoder_input=x_past,
                                                            additional_input=encoder_additional)
 
-            decoder_output = self.decoder(x_future=x_future, encoder_output=encoder2decoder)
+            decoder_output = self.decoder(x_future=x_future, encoder_output=encoder2decoder,
+                                          pos_idx=(x_past.shape[1], x_past.shape[1] + self.n_prediction_steps))
 
             if self.has_temporal_fusion:
                 decoder_output = self.temporal_fusion(encoder_output=encoder_output,
@@ -652,6 +655,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
 
                     decoder_output = self.decoder(x_future,
                                                   encoder_output=encoder2decoder,
+                                                  pos_idx=(x_past.shape[1] + idx_pred, x_past.shape[1] + idx_pred + 1),
                                                   cache_intermediate_state=True,
                                                   incremental_update=idx_pred > 0)
 
@@ -741,6 +745,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
 
                     decoder_output = self.decoder(x_future,
                                                   encoder_output=encoder2decoder,
+                                                  pos_idx=(x_past.shape[1]+idx_pred, x_past.shape[1] + idx_pred+1),
                                                   cache_intermediate_state=True,
                                                   incremental_update=idx_pred > 0)
                     if self.has_temporal_fusion:
@@ -753,7 +758,7 @@ class ForecastingSeq2SeqNet(ForecastingNet):
                                                               past_observed_values=past_observed_values,
                                                               decoder_length=idx_pred + 1,
                                                               static_embedding=x_static,
-                                                              )[:, -1:]
+                                                              )
 
                     net_output = self.head(decoder_output)
                     samples = self.pred_from_net_output(net_output).cpu()

autoPyTorch/pipeline/components/setup/network_backbone/forecasting_backbone/cells.py
@@ -590,6 +590,7 @@ class StackedDecoder(nn.Module):
     def forward(self,
                 x_future: Optional[torch.Tensor],
                 encoder_output: List[torch.Tensor],
+                pos_idx: Optional[Tuple[int]] = None,
                 cache_intermediate_state: bool = False,
                 incremental_update: bool = False
                 ) -> torch.Tensor:
@@ -599,25 +600,28 @@ class StackedDecoder(nn.Module):
             if self.decoder_has_hidden_states[i]:
                 if incremental_update:
                     hx = self.cached_intermediate_state[i]
-                    fx, hx = decoder_i(x_future=x, encoder_output=hx)
+                    fx, hx = decoder_i(x_future=x, encoder_output=hx, pos_idx=pos_idx)
                 else:
-                    fx, hx = decoder_i(x_future=x, encoder_output=encoder_output[i])
+                    fx, hx = decoder_i(x_future=x, encoder_output=encoder_output[i], pos_idx=pos_idx)
             else:
                 if incremental_update:
-                    x_all = torch.cat([self.cached_intermediate_state[i], x], dim=1)
-                    fx = decoder_i(x_all, encoder_output=encoder_output[i])[:, -1:]
+                    # in this case, we only have Transformer, thus x_all needs to be None value!
+                    # TODO make this argument clearer!
+                    #x_all = torch.cat([self.cached_intermediate_state[i], x], dim=1)
+                    fx = decoder_i(x, encoder_output=encoder_output[i], pos_idx=pos_idx)
                 else:
-                    fx = decoder_i(x, encoder_output=encoder_output[i])
+                    fx = decoder_i(x, encoder_output=encoder_output[i], pos_idx=pos_idx)
             skip_id = f'skip_connection_{block_id}'
             if self.skip_connection and skip_id in self.decoder and x is not None:
                 fx = self.decoder[skip_id](fx, x)
             if cache_intermediate_state:
                 if self.decoder_has_hidden_states[i]:
                     self.cached_intermediate_state[i] = hx
-                else:
-                    if incremental_update:
-                        self.cached_intermediate_state[i] = x_all
-                    else:
-                        self.cached_intermediate_state[i] = x
+                    #TODO consider if there are other case that could make use of cached intermediate states
+                # else:
+                #    if incremental_update:
+                #        self.cached_intermediate_state[i] = x_all
+                #    else:
+                #        self.cached_intermediate_state[i] = x
             x = fx
         return x

autoPyTorch/pipeline/components/setup/network_backbone/forecasting_backbone/components_util.py
@@ -1,7 +1,7 @@
 import math
 from sklearn.base import BaseEstimator
 
-from typing import Any, Dict, NamedTuple
+from typing import Any, Dict, NamedTuple, Optional, Tuple
 
 import torch
 from torch import nn
@@ -71,13 +71,14 @@ def build_transformer_layers(d_model: int, config: Dict[str, Any], layer_type='e
     dropout = config.get('dropout', 0.0)
     activation = config['activation']
     layer_norm_eps = config['layer_norm_eps']
+    norm_first = config['norm_first']
     if layer_type == 'encoder':
         return nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward,
-                                          dropout=dropout, activation=activation,
+                                          dropout=dropout, activation=activation, norm_first=norm_first,
                                           layer_norm_eps=layer_norm_eps, batch_first=True)
     elif layer_type == 'decoder':
         return nn.TransformerDecoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward,
-                                          dropout=dropout, activation=activation,
+                                          dropout=dropout, activation=activation, norm_first=norm_first,
                                           layer_norm_eps=layer_norm_eps, batch_first=True)
     else:
         raise ValueError('layer_type must be encoder or decoder!')
@@ -115,15 +116,19 @@ class PositionalEncoding(nn.Module):
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
-    def forward(self, x):
+    def forward(self, x, pos_idx:Optional[Tuple[int]] = None):
         r"""Inputs of forward function
         Args:
             x: the sequence fed to the positional encoder model (required).
         Shape:
             x: [batch size, sequence length embed dim]
+            pos_idx: positional index, indicating the index of the current
             output: [batch size, sequence length, embed dim]
         Examples:
             >>> output = pos_encoder(x)
         """
-        x = x + self.pe[:, :x.size(1), :]
+        if pos_idx is None:
+            x = x + self.pe[:, :x.size(1), :]
+        else:
+            x = x + self.pe[:, pos_idx[0]: pos_idx[1], :]
         return self.dropout(x)

autoPyTorch/pipeline/components/setup/network_backbone/forecasting_backbone/forecasting_decoder/NBEATSDecoder.py
@@ -74,7 +74,7 @@ class NBEATSBLock(DecoderNetwork):
         if self.use_dropout:
             layers.append(nn.Dropout(self.dropout_rate))
 
-    def forward(self, x_future: Optional[torch.Tensor], encoder_output: torch.Tensor):
+    def forward(self, x_future: Optional[torch.Tensor], encoder_output: torch.Tensor, pos_idx: Optional[Tuple[int]] = None):
         if self.backcast_head is None and self.forecast_head is None:
             # used to compute head dimensions
             return self.backbone(encoder_output)

autoPyTorch/pipeline/components/setup/network_backbone/forecasting_backbone/forecasting_decoder/TransformerDecoder.py
@@ -16,7 +16,7 @@ from autoPyTorch.datasets.base_dataset import BaseDatasetPropertiesType
 from autoPyTorch.pipeline.components.base_component import BaseEstimator
 from autoPyTorch.utils.common import add_hyperparameter
 
-from autoPyTorch.pipeline.components.setup.network_backbone.\
+from autoPyTorch.pipeline.components.setup.network_backbone. \
     forecasting_backbone.forecasting_decoder.base_forecasting_decoder import (
     BaseForecastingDecoder,
     DecoderProperties
@@ -40,16 +40,18 @@ class _TransformerDecoder(DecoderNetwork):
                  use_layer_norm_output: bool,
                  dropout_pd: float = 0.0,
                  layer_norm_eps_output: Optional[float] = None,
-                 n_prediction_steps:int = 1,
+                 n_prediction_steps: int = 1,
                  lagged_value: Optional[Union[List, np.ndarray]] = None):
         super().__init__()
         self.lagged_value = lagged_value
         in_features = in_features
 
-        self.input_layer = [nn.Linear(in_features, d_model, bias=False)]
+        # self.input_layer = [nn.Linear(in_features, d_model, bias=False)]
+        self.input_layer = nn.Linear(in_features, d_model, bias=False)
+
+        self.use_positional_decoder = use_positional_decoder
         if use_positional_decoder:
-            self.input_layer.append(PositionalEncoding(d_model, dropout_pd))
-        self.input_layer = nn.Sequential(*self.input_layer)
+            self.pos_encoding = PositionalEncoding(d_model, dropout_pd)
 
         self.use_layer_norm_output = use_layer_norm_output
 
@@ -62,8 +64,10 @@ class _TransformerDecoder(DecoderNetwork):
                                                                 norm=norm)
         self.tgt_mask = nn.Transformer.generate_square_subsequent_mask(n_prediction_steps)
 
-    def forward(self, x_future: torch.Tensor, encoder_output: torch.Tensor):
+    def forward(self, x_future: torch.Tensor, encoder_output: torch.Tensor, pos_idx: Optional[Tuple[int]] = None):
         output = self.input_layer(x_future)
+        if self.use_positional_decoder:
+            output = self.pos_encoding(output, pos_idx)
         if self.training:
             output = self.transformer_decoder_layers(output, encoder_output,
                                                      tgt_mask=self.tgt_mask.to(encoder_output.device))
@@ -110,7 +114,9 @@ class ForecastingTransformerDecoder(BaseForecastingDecoder):
 
     @staticmethod
     def decoder_properties():
-        return DecoderProperties(recurrent=True, lagged_input=True, mask_on_future_target=True)
+        return DecoderProperties(recurrent=True,
+                                 lagged_input=True,
+                                 mask_on_future_target=True)
 
     def fit(self, X: Dict[str, Any], y: Any = None) -> BaseEstimator:
         self.transformer_encoder_kwargs = X['transformer_encoder_kwargs']
@@ -148,6 +154,10 @@ class ForecastingTransformerDecoder(BaseForecastingDecoder):
             HyperparameterSearchSpace(hyperparameter='d_feed_forward_log',
                                       value_range=(6, 12),
                                       default_value=7),
+            norm_first: HyperparameterSearchSpace =
+            HyperparameterSearchSpace(hyperparameter="norm_first",
+                                      value_range=(True, False),
+                                      default_value=True),
             layer_norm_eps: HyperparameterSearchSpace =
             HyperparameterSearchSpace(hyperparameter='layer_norm_eps',
                                       value_range=(1e-7, 1e-3),
@@ -185,6 +195,7 @@ class ForecastingTransformerDecoder(BaseForecastingDecoder):
         cs = CS.ConfigurationSpace()
 
         add_hyperparameter(cs, activation, CategoricalHyperparameter)
+        add_hyperparameter(cs, norm_first, CategoricalHyperparameter)
 
         min_transformer_layers, max_transformer_layers = num_layers.value_range
 

autoPyTorch/pipeline/components/setup/network_backbone/forecasting_backbone/forecasting_decoder/components.py
@@ -21,7 +21,7 @@ class DecoderBlockInfo(NamedTuple):
 
 
 class DecoderNetwork(nn.Module):
-    def forward(self, x_future: torch.Tensor, encoder_output: torch.Tensor):
+    def forward(self, x_future: torch.Tensor, encoder_output: torch.Tensor, pos_idx: Optional[Tuple[int]] = None):
         """
         Base forecasting Decoder Network, its output needs to be a 3-d Tensor:
 