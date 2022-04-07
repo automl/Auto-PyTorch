671edc57216406fc29c7ecd042ab776a9f0b0950
Ravin Kohli <kohliravin7@gmail.com>
2022-04-07 10:43:26 +0200

    add updates for apt1.0+reg_cocktails

--- autoPyTorch/api/base_task.py
+++ autoPyTorch/api/base_task.py
@@ -125,6 +125,167 @@ def get_search_updates(categorical_indicator: List[bool]) -> HyperparameterSearc
 
     search_space_updates = HyperparameterSearchSpaceUpdates()
 
+    # architecture head
+    search_space_updates.append(
+        node_name='network_head',
+        hyperparameter='__choice__',
+        value_range=['no_head'],
+        default_value='no_head',
+    )
+    search_space_updates.append(
+        node_name='network_head',
+        hyperparameter='no_head:activation',
+        value_range=['relu'],
+        default_value='relu',
+    )
+
+    # weights initialisation
+    search_space_updates.append(
+        node_name='network_init',
+        hyperparameter='__choice__',
+        value_range=['NoInit'],
+        default_value='NoInit',
+    )
+    search_space_updates.append(
+        node_name='network_init',
+        hyperparameter='NoInit:bias_strategy',
+        value_range=['Zero'],
+        default_value='Zero',
+    )
+
+    # backbone architecture choices
+    search_space_updates.append(
+        node_name='network_backbone',
+        hyperparameter='__choice__',
+        value_range=['ShapedResNetBackbone'],
+        default_value='ShapedResNetBackbone',
+    )
+
+    # resnet backbone
+    search_space_updates.append(
+        node_name='network_backbone',
+        hyperparameter='ShapedResNetBackbone:resnet_shape',
+        value_range=['funnel'],
+        default_value='funnel',
+    )
+    search_space_updates.append(
+        node_name='network_backbone',
+        hyperparameter='ShapedResNetBackbone:num_groups',
+        value_range=[1, 4],
+        default_value=2,
+    )
+    search_space_updates.append(
+        node_name='network_backbone',
+        hyperparameter='ShapedResNetBackbone:blocks_per_group',
+        value_range=[1, 3],
+        default_value=2,
+    )
+    search_space_updates.append(
+        node_name='network_backbone',
+        hyperparameter='ShapedResNetBackbone:output_dim',
+        value_range=[32, 512],
+        default_value=64,
+        log=True
+    )
+    search_space_updates.append(
+        node_name='network_backbone',
+        hyperparameter='ShapedResNetBackbone:max_units',
+        value_range=[32, 512],
+        default_value=64,
+        log=True
+    )
+    search_space_updates.append(
+        node_name='network_backbone',
+        hyperparameter='ShapedResNetBackbone:activation',
+        value_range=['relu'],
+        default_value='relu',
+    )
+
+    # training updates
+    # lr scheduler
+    search_space_updates.append(
+        node_name='lr_scheduler',
+        hyperparameter='__choice__',
+        value_range=['CosineAnnealingWarmRestarts'],
+        default_value='CosineAnnealingWarmRestarts',
+    )
+    search_space_updates.append(
+        node_name='lr_scheduler',
+        hyperparameter='CosineAnnealingWarmRestarts:n_restarts',
+        value_range=[1, 3],
+        default_value=2,
+    )
+    # optimizer
+    search_space_updates.append(
+        node_name='optimizer',
+        hyperparameter='__choice__',
+        value_range=['AdamWOptimizer'],
+        default_value='AdamWOptimizer',
+    )
+    # adamw
+    search_space_updates.append(
+        node_name='optimizer',
+        hyperparameter='AdamWOptimizer:lr',
+        value_range=[1e-4, 1e-1],
+        default_value=1e-3,
+        log=True
+    )
+    search_space_updates.append(
+        node_name='optimizer',
+        hyperparameter='AdamWOptimizer:beta1',
+        value_range=[0.9],
+        default_value=0.9,
+    )
+    search_space_updates.append(
+        node_name='optimizer',
+        hyperparameter='AdamWOptimizer:beta2',
+        value_range=[0.999],
+        default_value=0.999,
+    )
+    search_space_updates.append(
+        node_name='data_loader',
+        hyperparameter='batch_size',
+        value_range=[16, 512],
+        default_value=128,
+        log=True
+    )
+
+    # preprocessing
+    if has_numerical_features:
+        search_space_updates.append(
+            node_name='feature_preprocessor',
+            hyperparameter='__choice__',
+            value_range=['NoFeaturePreprocessor', 'TruncatedSVD'],
+            default_value='NoFeaturePreprocessor',
+        )
+        search_space_updates.append(
+            node_name='feature_preprocessor',
+            hyperparameter='TruncatedSVD:target_dim',
+            value_range=[0.1, 0.9],
+            default_value=0.4,
+        )
+        search_space_updates.append(
+            node_name='imputer',
+            hyperparameter='numerical_strategy',
+            value_range=['mean'],
+            default_value='mean',
+        )
+        search_space_updates.append(
+            node_name='scaler',
+            hyperparameter='__choice__',
+            value_range=['StandardScaler'],
+            default_value='StandardScaler',
+        )
+
+    if has_cat_features:
+        search_space_updates.append(
+            node_name='encoder',
+            hyperparameter='__choice__',
+            value_range=['OneHotEncoder'],
+            default_value='OneHotEncoder',
+        )
+
+
     return search_space_updates
 
 
