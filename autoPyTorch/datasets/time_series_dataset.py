62757b1996e56683e7d2e46b1f1b873ca70e0d7d
Difan Deng <33290713+dengdifan@users.noreply.github.com>
2022-08-08 19:02:45 +0200

    Update autoPyTorch/datasets/time_series_dataset.py
    Co-authored-by: Ravin Kohli <13005107+ravinkohli@users.noreply.github.com>

--- autoPyTorch/datasets/time_series_dataset.py
+++ autoPyTorch/datasets/time_series_dataset.py
@@ -699,8 +699,8 @@ class TimeSeriesForecastingDataset(BaseDataset, ConcatDataset):
                 valid_splits.append(split)
 
         if len(valid_splits) == 0:
-            raise ValueError('The forecasting horizon setting is unsuited for the Current dataset, please '
-                             'considering reducing n_prediction_steps')
+            raise ValueError(f'The passed value for {n_prediction_steps} is unsuited for the current dataset, please '
+                             'consider reducing n_prediction_steps')
 
         self.splits = valid_splits
 
