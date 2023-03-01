2e1bd3b1dfb21cd3c6d321e22259317955643675
Ravin Kohli <kohliravin7@gmail.com>
2023-03-01 09:26:24 +0100

    make it work faster on GPU

--- autoPyTorch/pipeline/components/training/trainer/base_trainer_choice.py
+++ autoPyTorch/pipeline/components/training/trainer/base_trainer_choice.py
@@ -342,7 +342,7 @@ class TrainerChoice(autoPyTorchChoice):
             numerical_columns=X['dataset_properties']['numerical_columns'] if 'numerical_columns' in X[
                 'dataset_properties'] else None,
         )
-        # self.logger.debug(f"Choice prepare finished at: {time.time()}, took {time.time() - prepare_timestamp}")
+        self.logger.debug(f"Running on device: {get_device_from_fit_dictionary(X)}")
         total_parameter_count, trainable_parameter_count = self.count_parameters(X['network'])
         self.run_summary = RunSummary(
             total_parameter_count,
